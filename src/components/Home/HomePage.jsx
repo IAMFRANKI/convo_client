19fd9b8407d36d63f572e28ff46383ef36dab777
frank <thmsfrank04@gmail.com>
2024-04-08 21:04:34 +0300

    Homepage setup

---
+++ src/components/Home/HomePage.jsx
@@ -0,0 +1,50 @@
+
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+function HomePage() {
+  const [theme, setTheme] = useState('dark');
+
+  const toggleTheme = () => {
+    setTheme(prevTheme => (prevTheme === 'dark' ? 'light' : 'dark'));
+  };
+
+  return (
+    <div className={`container ${theme}-theme`}>
+      <div className="navbar">
+        <div className="profile-icon">
+          <Link to="/profile">
+            <img src="profile-icon.png" alt="Profile" />
+          </Link>
+        </div>
+        <div className="nav-buttons">
+          <Link to="/" className="nav-button">
+            Home
+          </Link>
+          <Link to="/categories" className="nav-button">
+            Categories
+          </Link>
+        </div>
+      </div>
+      <div className="content">
+        <div className="theme-button">
+          <button onClick={toggleTheme}>Change Theme</button>
+        </div>
+        <div className="threads">
+          {/* Thread components will be rendered here */}
+          <div className="thread">
+            <h3>Thread Title 1</h3>
+            <p>Thread content goes here...</p>
+          </div>
+          <div className="thread">
+            <h3>Thread Title 2</h3>
+            <p>Thread content goes here...</p>
+          </div>
+          {/* Add more thread components as needed */}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
