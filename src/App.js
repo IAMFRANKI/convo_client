5a4b4ded5c99b6bad371b52e974d070782267b29
frank <thmsfrank04@gmail.com>
2024-04-08 22:11:41 +0300

    Router issue solved

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'; // Import BrowserRouter and Switch
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/Home/HomePage.jsx';
 // import CategoriesPage from './components/CategoriesPage';
 // import ProfilePage from './components/ProfilePage';
 
 function App() {
   return (
-    <Router> {/* Wrap your components with Router */}
-      <Switch> {/* Use Switch to render the first matching route */}
-        <Route exact path="/" component={HomePage} /> {/* Define route for HomePage */}
-        {/* Define routes for other pages */}
-        {/* <Route path="/categories" component={CategoriesPage} /> */}
-        {/* <Route path="/profile" component={ProfilePage} /> */}
-      </Switch>
+    <Router> 
+      <Routes> 
+      <Route path="/HomePage" element={<HomePage />} />
+      </Routes>
     </Router>
   );
 }
--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -1,6 +1,6 @@
-
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import './HomePage.css';
 
 function HomePage() {
   const [theme, setTheme] = useState('dark');
